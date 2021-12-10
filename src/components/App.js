c958a09e3c71c733afc615543b305c092635530b
Abnit Chauhan <abnitchauhanabnit@gmail.com>
2021-12-10 15:32:44 +0530

    Query and Mutation Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
-import logo from './../logo.svg';
-import '../styles/App.css';
+import React from 'react';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import {Route, Routes} from 'react-router-dom';
+import LinkList  from './LinkList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  // return <CreateLink />;
+return(
+  <div className = "center w85">
+    <Header />
+    <div className= "ph3 pv1 background-gray">
+      <Routes>
+        <Route exact path ="/" element={<LinkList />} />
+        <Route exact path = "/create" element={<CreateLink />} /> 
+      </Routes>
     </div>
-  );
-}
+  </div>
+);
+};
+
+
+// class App extends Component{
+//   render(){
+//     return<LinkList />;
+//   }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { setContext } from '@apollo/client/link/context'
+import { BrowserRouter } from 'react-router-dom';
+
 
 // 1
 import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client'
@@ -10,18 +13,37 @@ import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apol
 //2
 const httpLink = createHttpLink({uri: 'http://localhost:4000'});
 
-//3
-const client = new ApolloClient({
-  link : httpLink,
-  cache : new InMemoryCache()
+// //3
+// const client = new ApolloClient({
+//   link : httpLink,
+//   cache : new InMemoryCache()
+// });
+
+// 3
+const authLink = setContext((_, { headers }) => {
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQsImlhdCI6MTYzOTEyNjM2NX0.RgGQUdivzL-ltLbBjiA508b83az47Fx0a2sef9mzfWM';
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
 });
- 
-ReactDOM.render(
 
 //4
-  <ApolloProvider client = {client} >
-    <App />
-  </ApolloProvider>,
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+//5
+  <BrowserRouter>
+    <ApolloProvider client = {client} >
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
